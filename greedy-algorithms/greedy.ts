27e03f4b7a289d93f4d36cc47a860c1149d497d9
Stolarek <przemyslaw.stolarek1@gmail.com>
2023-05-24 12:53:25 +0200

    Implement activity selection problem solution

--- greedy-algorithms/greedy.ts
+++ greedy-algorithms/greedy.ts
@@ -16,7 +16,7 @@ interface Item {
   weight: number;
   value: number;
 }
-const knapsack = (items: Item[], capacity: number) => {
+const knapsack = (items: Item[], capacity: number): number => {
   // Sort items weight to value ratio
   const sortedSack = [...items].sort(
     (a, b) => b.value / b.weight - a.value / a.weight
@@ -47,3 +47,52 @@ knapsack(
   ],
   8
 );
+
+/*
+Problem: Activity Selection
+Description: You are given a set of activities with their start and finish times. Design an algorithm to select the maximum number of non-overlapping activities that can be performed, assuming a person can only work on one activity at a time.
+
+Example:
+Input:
+
+Activities:
+Activity 1: Start time = 1, Finish time = 4
+Activity 2: Start time = 3, Finish time = 5
+Activity 3: Start time = 0, Finish time = 2
+Activity 4: Start time = 5, Finish time = 7
+Activity 5: Start time = 8, Finish time = 9
+Output: Maximum number of activities that can be performed is 3. (Activity 3, Activity 2, and Activity 5)
+*/
+
+interface Activities {
+  start: number;
+  end: number;
+}
+
+const activitySelection = (activities: Activities[]): number => {
+  // We sort activities by end times in ascending order
+  const sortedActivities = [...activities].sort((a, b) => a.end - b.end);
+  let maxActivities = 0;
+  let prevEnd = -Infinity;
+  for (let i = 0; i < sortedActivities.length; i++) {
+    const start = sortedActivities[i].start;
+    const end = sortedActivities[i].end;
+
+    // now we check if start of current activity is greater than previous end
+    if (start > prevEnd) {
+      // if yes, then we assing to previous end current end
+      prevEnd = end;
+      maxActivities += 1;
+    }
+  }
+
+  return maxActivities;
+};
+
+activitySelection([
+  { start: 1, end: 4 },
+  { start: 3, end: 5 },
+  { start: 0, end: 2 },
+  { start: 5, end: 7 },
+  { start: 8, end: 9 },
+]);
